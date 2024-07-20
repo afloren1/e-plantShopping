ddd283eb0e518478e894adb42ec81a3959a4a312
afloren1 <bluemoon66063@gmail.com>
2024-07-19 20:56:37 -0400

    added code to add total amount in cart under CartSlice.jsx and added $ to CartItem.jsx also fixed a margin in CartItem.css

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -9,10 +9,11 @@ export const CartSlice = createSlice({
     addItem: (state, action) => {
     const { name, image, cost } = action.payload
     const existingItem = state.items.find(item => item.name === name);
+    const numericCost = parseFloat(cost.replace('$', ''));
     if (existingItem) {
         existingItem.quantity += 1;
     } else {
-        state.items.push({ name, image, cost, quantity: 1});
+        state.items.push({ name, image, cost: numericCost, quantity: 1});
     }
     },
     removeItem: (state, action) => {
